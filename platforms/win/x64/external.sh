404714490ef4bdd4d5b1d2ef36116245cd1c3f83
Jason Millard <jsm174@gmail.com>
2024-09-10 17:39:11 -0400

    misc: add support for compiling hidapi win x64/x86

--- platforms/win/x64/external.sh
+++ platforms/win/x64/external.sh
@@ -5,11 +5,13 @@ set -e
 CARGS_SHA=5949a20a926e902931de4a32adaad9f19c76f251
 SOCKPP_SHA=e6c4688a576d95f42dd7628cefe68092f6c5cd0f
 LIBSERIALPORT_SHA=fd20b0fc5a34cd7f776e4af6c763f59041de223b
+HIDAPI_SHA=d0732cda906ad07b7e1ef93f1919035643620435
 
 echo "Building libraries..."
 echo "  CARGS_SHA: ${CARGS_SHA}"
 echo "  SOCKPP_SHA: ${SOCKPP_SHA}"
 echo "  LIBSERIALPORT_SHA: ${LIBSERIALPORT_SHA}"
+echo "  HIDAPI_SHA: ${HIDAPI_SHA}"
 echo ""
 
 if [ -z "${BUILD_TYPE}" ]; then
@@ -66,4 +68,19 @@ patch libserialport.vcxproj < ../../platforms/win/x64/libserialport/001.patch
 msbuild.exe libserialport.sln -p:Configuration=Release -p:Platform=x64
 cp x64/Release/*.lib ../../third-party/build-libs/win/x64
 cp x64/Release/*.dll ../../third-party/runtime-libs/win/x64
+cd ..
+
+#
+# build hdiapi and copy to platform/arch
+#
+
+curl -sL https://github.com/libusb/hidapi/archive/${HIDAPI_SHA}.zip -o hidapi.zip
+unzip hidapi.zip
+cd hidapi-$HIDAPI_SHA
+patch -p1 < ../../platforms/win/x64/hidapi/001.patch
+cmake -G "Visual Studio 17 2022" -B build
+cmake --build build --config ${BUILD_TYPE}
+cp -r hidapi ../../third-party/include/
+cp build/src/windows/${BUILD_TYPE}/*.lib ../../third-party/build-libs/win/x64/
+cp build/src/windows/${BUILD_TYPE}/*.dll ../../third-party/runtime-libs/win/x64/
 cd ..
