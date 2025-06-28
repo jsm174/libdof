f9f74216cef87d82843fbc3156e2e392f6cd5892
Jason Millard <jsm174@gmail.com>
2025-06-28 19:24:33 -0400

    libftdi1: update cmake minimum for compilation on newer linux systems

--- platforms/linux/aarch64/external.sh
+++ platforms/linux/aarch64/external.sh
@@ -74,6 +74,7 @@ curl -sL "http://developer.intra2net.com/git/?p=libftdi;a=snapshot;h=${LIBFTDI_S
 tar xzf libftdi-${LIBFTDI_SHA}.tar.gz
 mv libftdi-${LIBFTDI_SHA:0:7} libftdi
 cd libftdi
+sed -i.bak 's/cmake_minimum_required([^)]*)/cmake_minimum_required(VERSION 3.10)/' CMakeLists.txt
 cmake \
    -DFTDI_EEPROM=OFF \
    -DEXAMPLES=OFF \
